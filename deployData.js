9fe3cf6d42d5100f62281d54d3c0ac48aeb42b86
Khoirul Ansori <khoirulansori407@gmail.com>
2025-01-31 10:29:15 +0700

    first repo

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Khoirul Ansori'; 
+
+const studentId = 'FS11534226'; 
+
+const NetlifyDeployUrl = 'https://super-eclair-81d1a7.netlify.app/'; 
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
